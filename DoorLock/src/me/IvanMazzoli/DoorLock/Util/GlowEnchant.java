7e2c1574e75d05e1174df88caf48ac259a554fa5
Ivan Mazzoli <dreadtank27@gmail.com>
2014-07-07 00:19:09 +0200

    Added custom enchant and Lockpick item

---
+++ DoorLock/src/me/IvanMazzoli/DoorLock/Util/GlowEnchant.java
@@ -0,0 +1,48 @@
+package me.IvanMazzoli.DoorLock.Util;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.enchantments.EnchantmentWrapper;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * @author Jordan
+ * https://github.com/JordanSicherman
+ */
+public class GlowEnchant extends EnchantmentWrapper {
+
+	public GlowEnchant(int id) {
+		super(id);
+	}
+
+	@Override
+	public boolean canEnchantItem(ItemStack item) {
+		return true;
+	}
+
+	@Override
+	public boolean conflictsWith(Enchantment other) {
+		return false;
+	}
+
+	@Override
+	public EnchantmentTarget getItemTarget() {
+		return null;
+	}
+
+	@Override
+	public int getMaxLevel() {
+		return 10;
+	}
+
+	@Override
+	public String getName() {
+		return "Glow";
+	}
+
+	@Override
+	public int getStartLevel() {
+		return 1;
+	}
+
+}
